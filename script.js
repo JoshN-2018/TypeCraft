eb885cea6b6ff7d02d66d39cf7f41ada9209f614
Joshua Noon <josh.noon1@gmail.com>
2019-01-11 14:03:17 +0100

    done

--- script.js
+++ script.js
@@ -20,14 +20,82 @@ var ratioFour = 1.414 /* ratioOne = Augmented fourth */
 var ratio = 1.2
 
 // typographic levels
-var levelMinTwo = ratio
-var levelMinOne = ratio
+var levelMinTwo = ratio / ratio / ratio
+var levelMinOne = ratio / ratio
 var levelOne = ratio
 var levelThree = ratio * ratio * ratio
 var levelFive = ratio * ratio * ratio * ratio * ratio
 var levelSix = ratio * ratio * ratio * ratio * ratio * ratio
 
+// Run function on DOM load
+function breakHandlerInitial() {
+   // determine screen breakpoints
+   if(window.innerWidth > breakOne && window.innerWidth < breakTwo) {
+      screenBase = 2
+      ratio = ratioTwo
+   }
+   if(window.innerWidth > breakTwo && window.innerWidth < breakThree) {
+      screenBase = 3
+      ratio = ratioThree
+   }
+   if(window.innerWidth > breakThree) {
+      screenBase = 4
+      ratio = ratioFour
+   }
+   if(window.innerWidth < breakOne) {
+      screenBase = 1
+      ratio = ratioOne
+   }
 
+   // level update
+   levelMinTwo = ratio / ratio / ratio
+   levelMinOne = ratio / ratio
+   levelOne = ratio
+   levelThree = ratio * ratio * ratio
+   levelFive = ratio * ratio * ratio * ratio * ratio
+   levelSix = ratio * ratio * ratio * ratio * ratio * ratio
+
+
+   // execute based on break breakpoints
+   if(screenBase === 2) {
+      document.documentElement.style.setProperty('--level-min-two', levelMinTwo + 'px');
+      document.documentElement.style.setProperty('--level-min-one', levelMinOne + 'px');
+      document.documentElement.style.setProperty('--level-one', levelOne + 'px');
+      document.documentElement.style.setProperty('--level-three', levelThree + 'px');
+      document.documentElement.style.setProperty('--level-five', levelFive + 'px');
+      document.documentElement.style.setProperty('--level-six', levelSix + 'px');
+   }
+   if(screenBase === 3) {
+      document.documentElement.style.setProperty('--level-min-two', levelMinTwo + 'px');
+      document.documentElement.style.setProperty('--level-min-one', levelMinOne + 'px');
+      document.documentElement.style.setProperty('--level-one', levelOne + 'px');
+      document.documentElement.style.setProperty('--level-three', levelThree + 'px');
+      document.documentElement.style.setProperty('--level-five', levelFive + 'px');
+      document.documentElement.style.setProperty('--level-six', levelSix + 'px');
+   }
+   if(screenBase === 4) {
+      document.documentElement.style.setProperty('--level-min-two', levelMinTwo + 'px');
+      document.documentElement.style.setProperty('--level-min-one', levelMinOne + 'px');
+      document.documentElement.style.setProperty('--level-one', levelOne + 'px');
+      document.documentElement.style.setProperty('--level-three', levelThree + 'px');
+      document.documentElement.style.setProperty('--level-five', levelFive + 'px');
+      document.documentElement.style.setProperty('--level-six', levelSix + 'px');
+   }
+   if(screenBase === 1) {
+      document.documentElement.style.setProperty('--level-min-two', levelMinTwo + 'px');
+      document.documentElement.style.setProperty('--level-min-one', levelMinOne + 'px');
+      document.documentElement.style.setProperty('--level-one', levelOne + 'px');
+      document.documentElement.style.setProperty('--level-three', levelThree + 'px');
+      document.documentElement.style.setProperty('--level-five', levelFive + 'px');
+      document.documentElement.style.setProperty('--level-six', levelSix + 'px');
+   }
+}
+
+// Call function
+breakHandlerInitial();
+
+
+// Function to be run on window resize
 function breakHandler() {
 
    // determine screen breakpoints
@@ -49,8 +117,8 @@ function breakHandler() {
    }
 
    // level update
-   levelMinTwo = ratio
-   levelMinOne = ratio
+   levelMinTwo = ratio / ratio / ratio
+   levelMinOne = ratio / ratio
    levelOne = ratio
    levelThree = ratio * ratio * ratio
    levelFive = ratio * ratio * ratio * ratio * ratio
@@ -98,12 +166,3 @@ function breakHandler() {
 // this listens for 'resize' events (first argument) on the window object, if one occurs it calls the 'breakHandler' function (second argument).
 
 window.addEventListener('resize', breakHandler);
-
-
-/*
-var ratio = // ratio allocated to which quadrant dertermined by width
-var levOne = screenBase * ratio
-var levTwo = screenBase * ratio * ratio
-var levThree = screenBase * ratio * ratio * ratio
-var levFour = screenBase * ratio * ratio * ratio * ratio
-*/

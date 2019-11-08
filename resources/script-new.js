9a4fc6c83fa81eb1337651dcba1e21b9860cf910
Peter Goes <petergoes@gmail.com>
2019-11-08 17:27:49 +0100

    Add sizes array and activeBreakpoint variable
    We group all data related to sizes together. This way we can generate
    input elements when the user adds/removes sizes (breakpoints) more
    easily.
    We also keep a variable for the current breakpoint

--- resources/script-new.js
+++ resources/script-new.js
@@ -1,5 +1,15 @@
 import Content from './components/content.js'
 
+let activeBreakPoint = 720
+
+const sizes = [
+  { name: 'small',     baseType: 16, typeScale: 1.2,   breakpoint: 720 },
+  { name: 'medium',    baseType: 16, typeScale: 1.25,  breakpoint: 1024 },
+  { name: 'large',     baseType: 16, typeScale: 1.33,  breakpoint: 1440 },
+  { name: 'x-large',   baseType: 16, typeScale: 1.414, breakpoint: 1920 },
+  { name: 'xx-large',  baseType: 16, typeScale: 1.5,   breakpoint: Infinity },
+]
+
 // All avaiable classes
 const classes = ['hero', 'h1', 'h2', 'h3', 'h4', 'body-big', 'body', 'body-small', 'body-x-small'];
 
